1740342dd048e6a79dbc0871ff759853bba0893f
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-02-03 15:00:57 +0100

    add implementation for the task-list component

--- src/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.tasks.push(new Task(1, "Task1", true, "01/12/18"));
+        this.tasks.push(new Task(2, "Task2", false, "01/12/18"));
+        this.tasks.push(new Task(3, "Task3", false, "01/12/18"));
+    }
+
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'label-success' : 'label-primary';
+    }
+
+    onTaskChange(event, task) {
+        console.log('task has changed!');
+    }
 
 }
